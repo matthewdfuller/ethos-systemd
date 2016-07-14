525e0264a4a33ad34a8f9426b5ba5c3295abc1ae
Matt Fuller <fuller@adobe.com>
2016-07-14 11:41:20 -0400

    configuring aqua service with rds and internal elb for agent gateway communication

--- v1/opt/aqua/setup/leader/002-defaults.sh
+++ v1/opt/aqua/setup/leader/002-defaults.sh
@@ -5,8 +5,8 @@ source $DIR/../../../../lib/helpers.sh
 source /etc/environment
 
 etcd-set /aqua/config/db-path "${POSTGRES_DB_PATH::-5}"
-etcd-set /aqua/config/db-name "$POSTGRES_DB_NAME"
-etcd-set /aqua-audit/config/db-path "${POSTGRES_AUDIT_DB_PATH::-5}"
-etcd-set /aqua-audit/config/db-name "$POSTGRES_AUDIT_DB_NAME"
+etcd-set /aqua/config/db-name "scalock"
+etcd-set /aqua/config/db-audit-name "slk_audit"
 etcd-set /aqua/config/aqua-token "aquatoken"
 etcd-set /aqua/config/password "password"
+etcd-set /aqua/config/gateway-host "$AQUA_INTERNAL_ELB"
