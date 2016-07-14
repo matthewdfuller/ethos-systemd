af7440a21206e46f958d14290d6961a27f0bf674
Matt Fuller <fuller@adobe.com>
2016-07-14 18:27:00 -0400

    adding wait script for aqua agent and console

--- v1/opt/aqua/setup/leader/001-images.sh
+++ v1/opt/aqua/setup/leader/001-images.sh
@@ -3,6 +3,6 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 source $DIR/../../../../lib/helpers.sh
 
-etcd-set /images/scalock-gateway    "index.docker.io/behance/scalock-gateway:v1.1.1"
-etcd-set /images/scalock-agent      "index.docker.io/behance/scalock-agent:v1"
-etcd-set /images/scalock-server     "index.docker.io/behance/scalock-server:v1.1.1"
+etcd-set /images/scalock-gateway    "index.docker.io/behance/scalock-gateway:1.2.tp1"
+etcd-set /images/scalock-agent      "index.docker.io/behance/scalock-agent:1.2.tp1"
+etcd-set /images/scalock-server     "index.docker.io/behance/scalock-server:1.2.tp1"
