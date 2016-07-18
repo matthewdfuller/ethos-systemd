dfaaa7ac2451d76707b1390a5197f7ad243dda46
Matt Fuller <fuller@adobe.com>
2016-07-18 14:29:03 -0400

    preventing etcd auth overwrite

--- v1/setup/leader/005-etcd-auth.sh
+++ v1/setup/leader/005-etcd-auth.sh
@@ -26,21 +26,6 @@ sudo cat << EOF > $CRED_DIR/${1}.json
 EOF
 }
 
-# TODO: retrying of below etcd commands
-
-add_users $ETCDCTL_ROOT_USER $ETCDCTL_ROOT_PASSWORD
-add_users $ETCDCTL_READ_USER $ETCDCTL_READ_PASSWORD
-add_users $ETCDCTL_WRITE_USER $ETCDCTL_WRITE_PASSWORD
-
-# add root-user
-curl -L http://127.0.0.1:2379/v2/auth/users/${ETCDCTL_ROOT_USER} -XPUT -d "@$CRED_DIR/root.json"
-
-# add read-user
-curl -L http://127.0.0.1:2379/v2/auth/users/${ETCDCTL_READ_USER} -XPUT -d "@$CRED_DIR/read-user.json"
-
-# add write-user
-curl -L http://127.0.0.1:2379/v2/auth/users/${ETCDCTL_WRITE_USER} -XPUT -d "@$CRED_DIR/write-user.json"
-
 # read access
 etcdctl role add read-only
 etcdctl role grant read-only -path '/*' -read
@@ -49,13 +34,20 @@ etcdctl role grant read-only -path '/*' -read
 etcdctl role add read-write
 etcdctl role grant read-write -path '/*' -readwrite
 
+# TODO: retrying of below etcd commands
+
+add_users $ETCDCTL_ROOT_USER $ETCDCTL_ROOT_PASSWORD
+
+# add root-user
+curl -L http://127.0.0.1:2379/v2/auth/users/${ETCDCTL_ROOT_USER} -XPUT -d "@$CRED_DIR/root.json"
+
 # Give read-user read access
-sudo echo '{"user": "'${ETCDCTL_READ_USER}'", "grant": ["read-only"]}' > $CRED_DIR/read-only.json
-curl -L http://127.0.0.1:2379/v2/auth/users/${ETCDCTL_READ_USER} -XPUT -d "@$CRED_DIR/read-only.json"
+sudo echo '{"user": "'${ETCDCTL_READ_USER}'", "grant": ["read-only"], "password": "'$ETCDCTL_READ_PASSWORD'"}' > $CRED_DIR/read-user.json
+curl -L http://127.0.0.1:2379/v2/auth/users/${ETCDCTL_READ_USER} -XPUT -d "@$CRED_DIR/read-user.json"
 
 # Give read-write write access
-sudo echo '{"user": "'${ETCDCTL_WRITE_USER}'", "grant": ["read-write"]}' > $CRED_DIR/read-write.json
-curl -L http://127.0.0.1:2379/v2/auth/users/${ETCDCTL_WRITE_USER} -XPUT -d "@$CRED_DIR/read-write.json"
+sudo echo '{"user": "'${ETCDCTL_WRITE_USER}'", "grant": ["read-write"], "password": "'$ETCDCTL_WRITE_PASSWORD'"}' > $CRED_DIR/write-user.json
+curl -L http://127.0.0.1:2379/v2/auth/users/${ETCDCTL_WRITE_USER} -XPUT -d "@$CRED_DIR/write-user.json"
 
 # Enable authentication
 etcdctl auth enable
