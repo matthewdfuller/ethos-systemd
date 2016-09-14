0063fa3d2f6edfb4d0d2f9d203bf258d6322faf9
Matt Fuller <fuller@adobe.com>
2016-09-14 16:14:38 -0400

    adding updated marathon and mesos

--- v1/setup/leader/001-defaults.sh
+++ v1/setup/leader/001-defaults.sh
@@ -18,8 +18,8 @@ etcd-set /images/klam-ssh               "index.docker.io/behance/klam-ssh:v1"
 
 etcd-set /images/chronos                "index.docker.io/mesosphere/chronos:chronos-2.5.0-0.1.20160824153434.ubuntu1404-mesos-1.0.0"
 etcd-set /images/flight-director        "index.docker.io/behance/flight-director:c5e22bb3e7f2d2149b9744b9e0169ae756d5852a"
-etcd-set /images/marathon               "index.docker.io/mesosphere/marathon:v1.1.2"
-etcd-set /images/mesos-master           "index.docker.io/mesosphere/mesos-master:0.28.20.28.2-2.0.27.ubuntu1404"
+etcd-set /images/marathon               "index.docker.io/mesosphere/marathon:v1.3.0"
+etcd-set /images/mesos-master           "index.docker.io/mesosphere/mesos-master:1.0.11.0.1-2.0.93.ubuntu1404"
 etcd-set /images/zk-exhibitor           "index.docker.io/behance/docker-zk-exhibitor:v1.0.0"
 etcd-set /images/cfn-signal             "index.docker.io/behance/docker-cfn-bootstrap:v1.0.0"
 etcd-set /images/jenkins                "index.docker.io/jenkins:1.651.1"
@@ -30,7 +30,7 @@ etcd-set /images/capcom                 "index.docker.io/behance/capcom:3ddcfe36
 etcd-set /images/proxy                  "index.docker.io/nginx:1.9.5"
 etcd-set /images/control-proxy          "index.docker.io/behance/apigateway:v0.0.1"
 
-etcd-set /images/mesos-slave            "index.docker.io/mesosphere/mesos-slave:0.28.20.28.2-2.0.27.ubuntu1404"
+etcd-set /images/mesos-slave            "index.docker.io/mesosphere/mesos-slave:1.0.11.0.1-2.0.93.ubuntu1404"
 
 
 ######################
